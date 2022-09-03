144aaba34c9a38d9cece4ef05b8013c50ab40c3f
thoma <toma0805@yandex.ru>
2022-09-03 21:08:10 +0300

    added pagination

--- main.go
+++ main.go
@@ -46,6 +46,21 @@ type Search struct {
 	Results    Results
 }
 
+func (s *Search) IsLastPage() bool {
+	return s.NextPage >= s.TotalPages
+}
+
+func (s *Search) CurrentPage() int {
+	if s.NextPage == 1 {
+		return s.NextPage
+	}
+	return s.NextPage - 1
+}
+
+func (s *Search) PreviousPage() int {
+	return s.CurrentPage() - 1
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tpl.Execute(w, nil)
 }
@@ -98,6 +113,9 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	search.TotalPages = int(math.Ceil(float64(search.Results.TotalResults / pageSize)))
+	if ok := !search.IsLastPage(); ok {
+		search.NextPage++
+	}
 	err = tpl.Execute(w, search) // рендерим наш шаблон и передаем переменную search в качестве интерфейса данных
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
